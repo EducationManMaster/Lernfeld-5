f58d7f21d409bc7b45a94f69c049f1ace712439e
EducationManMaster <71923384+EducationManMaster@users.noreply.github.com>
2020-10-19 18:31:20 +0200

    Add files via upload

---
+++ sql/create/2_createDB.sql
@@ -0,0 +1,74 @@
+DROP DATABASE IF EXISTS krautundrueben;
+CREATE DATABASE IF NOT EXISTS krautundrueben;
+USE krautundrueben;
+
+
+CREATE TABLE KUNDE (
+    KUNDENNR        INTEGER NOT NULL,
+    NACHNAME        VARCHAR(50),
+    VORNAME         VARCHAR(50),
+    GEBURTSDATUM	  DATE,
+	 STRASSE         VARCHAR(50),
+	 HAUSNR			  VARCHAR(6),
+    PLZ             VARCHAR(5),
+    ORT             VARCHAR(50),
+    TELEFON         VARCHAR(25),
+    EMAIL           VARCHAR(50)
+    );
+
+CREATE TABLE ZUTAT(
+    ZUTATENNR			INTEGER NOT NULL,
+    BEZEICHNUNG      VARCHAR(50),
+    EINHEIT        	VARCHAR (25),
+    NETTOPREIS       DECIMAL(10,2),
+    BESTAND          INTEGER,
+    LIEFERANT			INTEGER,
+	 KALORIEN			INTEGER,
+	 KOHLENHYDRATE		DECIMAL (10,2),
+	 PROTEIN				DECIMAL(10,2)
+);
+
+CREATE TABLE BESTELLUNG (
+    BESTELLNR        INTEGER AUTO_INCREMENT NOT NULL,
+    KUNDENNR         INTEGER,
+    BESTELLDATUM     DATE,
+    RECHNUNGSBETRAG  DECIMAL(10,2),
+    PRIMARY KEY (BESTELLNR)
+);
+
+CREATE TABLE BESTELLUNGZUTAT (
+    BESTELLNR       INTEGER NOT NULL,
+    ZUTATENNR       INTEGER,
+    MENGE     		  INTEGER
+);
+
+CREATE TABLE LIEFERANT (
+    LIEFERANTENNR   INTEGER NOT NULL,
+    LIEFERANTENNAME VARCHAR(50),
+    STRASSE         VARCHAR(50),
+    HAUSNR			  VARCHAR(6),
+    PLZ             VARCHAR(5),
+    ORT             VARCHAR(50),
+    TELEFON			  VARCHAR(25),
+    EMAIL           VARCHAR(50)
+);
+
+/******************************************************************************/
+/***                              Primary Keys                              ***/
+/******************************************************************************/
+
+
+ALTER TABLE ZUTAT ADD PRIMARY KEY (ZUTATENNR);
+ALTER TABLE KUNDE ADD PRIMARY KEY (KUNDENNR);
+ALTER TABLE LIEFERANT ADD PRIMARY KEY (LIEFERANTENNR);
+ALTER TABLE BESTELLUNGZUTAT ADD PRIMARY KEY (BESTELLNR,ZUTATENNR);
+
+
+/******************************************************************************/
+/***                              Foreign Keys                              ***/
+/******************************************************************************/
+
+ALTER TABLE ZUTAT ADD FOREIGN KEY (LIEFERANT) REFERENCES LIEFERANT (LIEFERANTENNR);
+ALTER TABLE BESTELLUNGZUTAT ADD FOREIGN KEY (BESTELLNR) REFERENCES BESTELLUNG (BESTELLNR);
+ALTER TABLE BESTELLUNG ADD FOREIGN KEY (KUNDENNR) REFERENCES KUNDE (KUNDENNR);
+ALTER TABLE BESTELLUNGZUTAT ADD FOREIGN KEY (ZUTATENNR) REFERENCES ZUTAT (ZUTATENNR);
